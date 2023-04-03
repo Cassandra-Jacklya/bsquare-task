d9e3e0715bdcd03ae8691edef64ff01488046764
Cassandra-Jacklya <cassandrajacklya7@gmail.com>
2023-04-03 21:47:56 +0800

    first commit

--- chat.js
+++ chat.js
@@ -1,21 +1,32 @@
 const socket = new WebSocket('ws://localhost:8080');
+const chatWindow = document.getElementById('chat-window');
 
 socket.addEventListener('open', () => {
   socket.send('New user joined the chat');
 });
 
 socket.addEventListener('message', (event) => {
-  const message = event.data;
+  if (event.data instanceof Blob) {
+    event.data.text().then(txt => 
+      chatWindow.innerHTML += `<p>${txt}</p>`
+    );
+  }
+  else {
+    chatWindow.innerHTML += `<p>${event.data}</p>` ;
+  }
 });
 
 socket.addEventListener('close', () => {
-  // update UI to indicate user has left the chat
+  console.log("here");
+  socket.send('User has left the chat');
 });
+
 
 const messageForm = document.getElementById('message-form');
 const messageInput = document.getElementById('message-input');
 
 messageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
   const message = messageInput.value;
   socket.send(message);
   messageInput.value = '';
